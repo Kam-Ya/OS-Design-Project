7d9fe5f4ce87384e57d9072757d5fb559154deb1
Kam-Ya <kamranyaghoubian@gmail.com>
2024-11-23 22:43:44 -0500

    first version for moveFile

--- moveFile.h
+++ moveFile.h
@@ -1,6 +1,15 @@
 #ifndef MOVEFILE_H
 #define MOVEFILE_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
 
-
+void move(char *file, char *dest) {
+    if (rename(file, dest)) {
+        return;
+    } else {
+        printf("Error: %d\n", errno);
+    }
+}
 
 #endif
