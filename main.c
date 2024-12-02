01f345be08c2e2fa2c504d148b559f9a3b07e381
DavidL0228 <145606518+DavidL0228@users.noreply.github.com>
2024-12-02 13:23:32 -0500

    fixed main menu to detect errors, clear input buffer. added comments/formatting

--- main.c
+++ main.c
@@ -1,38 +1,65 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "createFile.h"
-#include "copyFile.h"
-#include "deleteFile.h"
-#include "moveFile.h"
-#include "renameFile.h"
-#include "textFile.h"
+#define _CRT_SECURE_NO_WARNINGS  // Disable warnings for unsafe functions like scanf in MSVC
+#include <stdio.h>              // Standard input/output library
+#include <stdlib.h>             // Standard library for functions like exit
+#include "createFile.h"         // Header file for the createFile function
+#include "copyFile.h"           // Header file for the copyFile function
+#include "deleteFile.h"         // Header file for the deleteFile function
+#include "moveFile.h"           // Header file for the moveFile function
+#include "renameFile.h"         // Header file for the renameFile function
+#include "textFile.h"           // Header file for the textFiles functions
 
-int main(){
-    int menu;
-while(1){
-    printf("Welcome to the file management system\nPlease enter what command you want to run.\n1 - createFile\n2 - copyFile\n3 - deleteFile\n4 - moveFile\n5 - renameFile\n6 - to menipulate a text file\n7 - to exit\n");
-    scanf("%d",&menu);
-    if (menu==1){
-        createFile();
-    }
-    else if (menu==2){
-        copyFile();
-    } else if (menu == 3) {
-        deleteFile();
-    } else if (menu == 4) {
-        move();
-    } else if(menu == 5) {
-        renameFile();
-    }
-    else if (menu == 6) {
-        textFiles();
-    }
-    else if (menu == 7) {
-        exit(0);
-    }
-    else {
-        printf("Please Enter a Valid Input")
-    }
+int main() {
+    int menu;  // Variable to store the user's menu selection
+    int c;     // Variable for clearing input buffer
 
+    // Infinite loop for continuously displaying the menu until the user exits
+    while (1) {
+        // Display the menu options to the user
+        printf("\nWelcome to the file management system\n");
+        printf("Please enter what command you want to run\n");
+        printf("1 - createFile\n");
+        printf("2 - copyFile\n");
+        printf("3 - deleteFile\n");
+        printf("4 - moveFile\n");
+        printf("5 - renameFile\n");
+        printf("6 - to manipulate a text file\n");
+        printf("7 - to exit\n");
+
+        // Read the user's menu selection
+        scanf("%d", &menu);
+
+        // Clear the input buffer to prevent issues with subsequent input
+        c = NULL;  // Reset the variable
+        while ((c = getchar()) != '\n' && c != EOF); // Consume all leftover characters in stdin
+
+        // Print the user's menu selection for confirmation
+        printf("You Picked: %d\n", menu);
+
+        // Execute the appropriate function based on the user's menu selection
+        if (menu == 1) {
+            createFile();  // Call the createFile function
+        }
+        else if (menu == 2) {
+            copyFile();    // Call the copyFile function
+        }
+        else if (menu == 3) {
+            deleteFile();  // Call the deleteFile function
+        }
+        else if (menu == 4) {
+            move();        // Call the move function
+        }
+        else if (menu == 5) {
+            renameFile();  // Call the renameFile function
+        }
+        else if (menu == 6) {
+            textFiles();   // Call the textFiles functions
+        }
+        else if (menu == 7) {
+            exit(0);       // Exit the program
+        }
+        else {
+            // Inform the user if they entered an invalid option
+            printf("\nPlease Enter a Valid Input\n");
+        }
     }
 }
