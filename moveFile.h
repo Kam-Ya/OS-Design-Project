dddcaa8a8c106e67904ccfb4e788ebaaad29ae43
david <daloover@lakeheadu.ca>
2024-12-02 23:20:50 -0500

    added error checking for moving files and renaming files

--- moveFile.h
+++ moveFile.h
@@ -7,9 +7,18 @@
 void move() {
     char from[50];
     char to[50];
+    FILE* file;
 
     printf("Enter the name of the file you want to move: ");
     scanf("%s", from);
+
+    // Check if the old file exists
+    file = fopen(from, "r");
+    if (file == NULL) {
+        printf("\nError: File '%s' does not exist.\n", from);
+        return;
+    }
+    fclose(file);
 
     printf("Enter the name of the files destination: ");
     scanf("%s", to);
--- renameFile.h
+++ renameFile.h
@@ -3,26 +3,43 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-void renameFile(){
+void renameFile() {
     char oldName[58];
     char newName[50];
+    FILE* file;
 
+    // Gets name of old file
     printf("\nWhat is the old name of the file: ");
     scanf("%s", oldName);
 
+    // Check if the old file exists
+    file = fopen(oldName, "r");
+    if (file == NULL) {
+        printf("\nError: File '%s' does not exist.\n", oldName);
+        return;
+    }
+    fclose(file);
+
+    //gets new name of file
     printf("\nWhat is the new name of the file: ");
     scanf("%s", newName);
 
-    if(rename(oldName,newName)==0){
-        printf("\nFile renamed to %s",newName);
-    }
-    else{
-        printf("\nFailed to rename file");
-    }
-    
 
+    // Check if the new file name is already taken
+    file = fopen(newName, "r");
+    if (file != NULL) {
+        printf( "\nError: A file with the name '%s' already exists.\n", newName);
+        fclose(file);
+        return;
+    }
+
+    // Attempt to rename the file
+    if (rename(oldName, newName) == 0) {
+        printf("\nFile renamed successfully to '%s'.\n", newName);
+    } else {
+        printf("\nFailed to rename file. Please check permissions and try again.\n");
+    }
 }
 
 #endif
